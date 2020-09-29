bbb525167e9fc496870e3f667fcde7c5184c465b
Quazi Rahmatullah <shaiun@outlook.com>
2020-09-29 23:33:12 +1300

    Introduce service class

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common';
-import { TasksController } from './tasks.controller';
+import {Module} from '@nestjs/common';
+import {TasksController} from './tasks.controller';
+import {TasksService} from './tasks.service';
 
 @Module({
-  controllers: [TasksController],
+    controllers: [TasksController],
+    providers: [TasksService],
 })
-export class TasksModule {}
+export class TasksModule {
+}
